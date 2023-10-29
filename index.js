0ca0b5341a3b5513f10b51f0a892ededc71ff421
paulthomas0 <paul.thomas@neutrinos.co>
2023-10-29 15:50:29 +0530

    Update for timing

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 
 const SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl'];
 const TOKEN_PATH = 'token.json';
 const CREDENTIALS_PATH = 'credentials.json';
+const MAX_RESULTS = 10;
+const RATE_LIMIT_DELAY = 60000; // 60 seconds
 
 function authenticate() {
     return new Promise((resolve, reject) => {
+        console.log('Loading client secret file...');
         fs.readFile(CREDENTIALS_PATH, (err, content) => {
-            if (err) return reject('Error loading client secret file:', err);
+            if (err) {
+                console.error('Error loading client secret file:', err);
+                return reject(err);
+            }
             
+            console.log('Authorizing with credentials...');
             authorize(JSON.parse(content), resolve);
         });
     });
 }
 
 function authorize(credentials, callback) {
-    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oauth2Client = new OAuth2(client_id, client_secret, redirect_uris[0]);
 
+    console.log('Checking for existing token...');
     fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oauth2Client, callback);
+        if (err) {
+            console.log('Token not found. Generating a new one...');
+            return getNewToken(oauth2Client, callback);
+        }
         
+        console.log('Using existing token.');
         oauth2Client.credentials = JSON.parse(token);
         callback(oauth2Client);
     });
@@ -45,7 +57,10 @@ function getNewToken(oauth2Client, callback) {
         rl.close();
 
         oauth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
+            if (err) {
+                console.error('Error retrieving access token', err);
+                return;
+            }
             
             oauth2Client.credentials = token;
             fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
@@ -59,16 +74,17 @@ function getNewToken(oauth2Client, callback) {
 }
 
 function listAndRemoveLikes(auth) {
-    const service = google.youtube({version: 'v3', auth});
+    const service = google.youtube({ version: 'v3', auth });
     
     let nextPageToken = undefined;
     let likedVideos = [];
 
     function fetchLikedVideos() {
+        console.log('Fetching liked videos...');
         service.videos.list({
             part: 'id,snippet',
             myRating: 'like',
-            maxResults: 50,
+            maxResults: MAX_RESULTS,
             pageToken: nextPageToken
         }, (err, response) => {
             if (err) {
@@ -76,19 +92,25 @@ function listAndRemoveLikes(auth) {
                 return;
             }
 
-            likedVideos = likedVideos.concat(response.data.items);
+            const currentBatch = response.data.items;
+            console.log(`Fetched ${currentBatch.length} liked videos.`);
+            likedVideos = likedVideos.concat(currentBatch);
             
             if (response.data.nextPageToken) {
                 nextPageToken = response.data.nextPageToken;
-                fetchLikedVideos();
+                setTimeout(fetchLikedVideos, RATE_LIMIT_DELAY);
             } else {
+                console.log(`Total liked videos to remove: ${likedVideos.length}`);
                 processLikedVideos();
             }
         });
     }
 
     function processLikedVideos() {
-        if (!likedVideos.length) return;
+        if (!likedVideos.length) {
+            console.log('All liked videos processed.');
+            return;
+        }
 
         const video = likedVideos.shift();
         console.log(`Removing like from video: ${video.snippet.title}`);
@@ -98,9 +120,9 @@ function listAndRemoveLikes(auth) {
         }, (err, response) => {
             if (err) {
                 console.error('Error:', err);
-            } else {
-                processLikedVideos();
-            }
+                return;
+            } 
+            setTimeout(processLikedVideos, RATE_LIMIT_DELAY / MAX_RESULTS); // spread out over 60 seconds
         });
     }
 
@@ -108,5 +130,6 @@ function listAndRemoveLikes(auth) {
 }
 
 authenticate().then(auth => {
+    console.log('Authentication successful.');
     listAndRemoveLikes(auth);
 }).catch(console.error);
